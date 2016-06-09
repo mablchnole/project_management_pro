7aae0d0fa66c1a1b1db6fce8f5bf75448fc4ccba
Lukas Udstuen <lukasudstuen@MacBook-Pro-de-Lukas.local>
2016-06-09 12:04:54 -0500

    now we can append to the div

---
+++ public/client_app.js
@@ -0,0 +1,18 @@
+var compName = ['Unicorn','Stardust', 'Gyroscope', 'Flicker', 'Prime Digital' ];
+
+
+
+
+$(document).ready(function(){
+$('#getProjectIn').click(function(){
+var html = document.createElement('div');
+html.id = "newDiv";
+html.textContent ="hello";
+$('body').append(html);
+
+
+
+
+});
+
+});
